1b70124ebde8f8a7686d973e380f132228e94470
Longs Pemun Gotar <developerlongs@gmail.com>
2024-09-17 11:05:04 +0100

    parsing json requests

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,19 +1,33 @@
 package data
 
 import (
-	"fmt"
+	"errors"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
-// Implement a MarshalJSON() method on the Runtime type so that it satisfies the
-// json.Marshaler interface. This should return the JSON-encoded value for the movie
-// runtime (in our case, it will return a string in the format "<runtime> mins").
-func (r Runtime) MarshalJSON() ([]byte, error) {
-	jsonValue := fmt.Sprintf("%d mins", r)
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
 
-	quotedJSONValue := strconv.Quote(jsonValue)
+	parts := strings.Split(unquotedJSONValue, " ")
 
-	return []byte(quotedJSONValue), nil
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
